ac558cc19dc1b7d3cd1f34a5188eeb11e70a58cb
ClariS <1457715339@qq.com>
2021-04-01 09:58:18 +0800

    🎨 style: 修正代码格式

--- __test__/db.spec.js
+++ __test__/db.spec.js
@@ -4,29 +4,29 @@ const fs = require("fs");
 jest.mock("fs");
 
 describe("db", () => {
-	afterEach(() => {
-		// afterEach 表示在每一个测试用例执行完后进行清除 mock 的操作，防止影响到后面的测试（比如后面也用"/xxx"）
-		fs.clearMocks();
-	});
+  afterEach(() => {
+    // afterEach 表示在每一个测试用例执行完后进行清除 mock 的操作，防止影响到后面的测试（比如后面也用"/xxx"）
+    fs.clearMocks();
+  });
   // 测试【读文件】
-	it("can read", async () => {
-		const data = [{ title: "hi", done: true }];
-		fs.setReadFileMock("/xxx", null, JSON.stringify(data));
-		const list = await db.read("/xxx");
-		expect(list).toStrictEqual(data); // 是否“深度相等”
-	});
+  it("can read", async () => {
+    const data = [{ title: "hi", done: true }];
+    fs.setReadFileMock("/xxx", null, JSON.stringify(data));
+    const list = await db.read("/xxx");
+    expect(list).toStrictEqual(data); // 是否“深度相等”
+  });
   // 测试【写文件】
-	it("can write", async () => {
-		let fakeFile;
-		fs.setWriteFileMock("/yyy", (path, data, callback) => {
-			fakeFile = data;
-			callback(null);
-		});
-		const list = [
-			{ title: "JOJO", done: true },
-			{ title: "DIO", done: false }
-		];
-		await db.write(list, "/yyy");
-		expect(fakeFile).toBe(JSON.stringify(list) + "\n");
-	});
+  it("can write", async () => {
+    let fakeFile;
+    fs.setWriteFileMock("/yyy", (path, data, callback) => {
+      fakeFile = data;
+      callback(null);
+    });
+    const list = [
+      { title: "JOJO", done: true },
+      { title: "DIO", done: false }
+    ];
+    await db.write(list, "/yyy");
+    expect(fakeFile).toBe(JSON.stringify(list) + "\n");
+  });
 });
--- db.js
+++ db.js
@@ -2,33 +2,33 @@ const homedir = require("os").homedir(); // nodejs get home directory
 const home = process.env.HOME || homedir; // process.env.HOME 读取用户设置的环境变量作为 HOME 目录：nodejs get home variable，如果没有设置，则和系统默认目录保持一致
 const fs = require("fs");
 const p = require("path");
-const dbPath = p.join(home, ".todo_db");
+const dbPath = p.join(home, ".todo_db"); // path.join() 方法会将所有给定的 path 片段连接到一起（使用平台特定的分隔符作为定界符），然后规范化生成的路径。
 
 const db = {
-	read(path = dbPath) {
-		return new Promise((resolve, reject) => {
+  read(path = dbPath) {
+    return new Promise((resolve, reject) => {
       // 参数 flag: a+ 表示：打开文件以进行读取和追加，如果文件不存在，则创建该文件。
-			fs.readFile(path, { flag: "a+" }, (error, data) => {
-				if (error) return reject(error);
-				let list;
-				try {
-					list = JSON.parse(data.toString());
-				} catch (error2) {
-					list = [];
-				}
-				resolve(list);
-			});
-		});
-	},
-	write(list, path = dbPath) {
-		return new Promise((resolve, reject) => {
-			const string = JSON.stringify(list);
-			fs.writeFile(path, string + "\n", (error) => {
-				if (error) return reject(error);
-				resolve();
-			});
-		});
-	}
+      fs.readFile(path, { flag: "a+" }, (error, data) => {
+        if (error) return reject(error);
+        let list;
+        try {
+          list = JSON.parse(data.toString());
+        } catch (error2) {
+          list = [];
+        }
+        resolve(list);
+      });
+    });
+  },
+  write(list, path = dbPath) {
+    return new Promise((resolve, reject) => {
+      const string = JSON.stringify(list);
+      fs.writeFile(path, string + "\n", (error) => {
+        if (error) return reject(error);
+        resolve();
+      });
+    });
+  }
 };
 
 module.exports = db;
